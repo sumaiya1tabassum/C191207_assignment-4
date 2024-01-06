61b107499399edf661c095edff76584a069da4f8
sumaiya1tabassum <154077963+sumaiya1tabassum@users.noreply.github.com>
2024-01-06 18:30:27 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const _ = require('underscore');
+const array=["Java","Javascript","Typescript"];
+const arr1=_.contains(array,"Java");
+const arr2=_.contains(array,"C++");
+console.log(arr1);
+console.log(arr2);
